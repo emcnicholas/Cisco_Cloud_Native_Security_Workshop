4243c0b1916cddc13d0a0ca0f6cf651e860b7e72
Ed McNicholas <edmcnich@cisco.com>
2021-09-23 12:25:48 -0400

    Update secure_workload_clusterrolebinding.tf

--- DEV/Applications/secure_workload_clusterrolebinding.tf
+++ DEV/Applications/secure_workload_clusterrolebinding.tf
@@ -59,12 +59,12 @@ data "kubernetes_service_account" "tetration_read_only" {
     name = "tetration.read.only"
   }
 }
-data "kubernetes_secret" "tetration_read_only" {
+data "kubernetes_secret" "tetration_read_only_name" {
   metadata {
     name = data.kubernetes_service_account.tetration_read_only.default_secret_name
   }
 }
-output "tetration_read_only_secret_name" {
-  value = data.kubernetes_secret.tetration_read_only
+output "tetration_read_only_secret_token" {
+  value = data.kubernetes_secret.tetration_read_only_name.data.token
   sensitive = true
 }
