38bd10b3ee31c9d239a490af2ba41c5a87f45d53
Ed McNicholas <edmcnich@cisco.com>
2021-09-23 11:21:08 -0400

    updated jenkinsfile

--- DEV/Applications/secure_workload_deploy.tf
+++ DEV/Applications/secure_workload_deploy.tf
@@ -1,10 +1,10 @@
 
 
 // Install the Secure Workload Daemonset //
-resource "null_resource" "update-kubeconfig" {
-  depends_on = []
-  provisioner "local-exec" {
-      working_dir = path.root
-      command = "aws eks --region ${var.region} update-kubeconfig --name ${local.eks_cluster_name}"
-  }
-}
+//resource "null_resource" "update-kubeconfig" {
+//  depends_on = []
+//  provisioner "local-exec" {
+//      working_dir = path.root
+//      command = "aws eks --region ${var.region} update-kubeconfig --name ${local.eks_cluster_name}"
+//  }
+//}
